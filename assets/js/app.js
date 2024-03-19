8b0d33d7917a447ede06a09f2ad62d1f9d47a163
danielecolaci <danielecolaci@outlook.com>
2024-03-19 15:39:50 +0100

    Get API to show in console 10 random mail with for loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,23 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            emailList: []
         }
+    },
+    methods: {
+        callApi() {
+            const emailList = [];
+            for (let i = 0; i < 10; i++) {
+                emailList.push(
+                    axios
+                        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((response) => {
+                            console.log(response.data.response);
+                        }))
+            }
+        }
+    },
+    mounted() {
+        this.callApi()
     }
 }).mount('#app')
